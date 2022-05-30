57559f813e281da5f2c8b7af9426ab237cb9a9f9
Michael Bryant <mbryant2025@gmail.com>
2022-05-29 23:18:14 -0400

    basic matrix library

--- src/testMatrix.java
+++ src/testMatrix.java
@@ -1,6 +1,30 @@
+import org.ejml.data.DMatrix3x3;
+import org.ejml.dense.fixed.CommonOps_DDF3;
+
 public class testMatrix {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        DMatrix3x3 a = new DMatrix3x3();
+        DMatrix3x3 b = new DMatrix3x3();
+
+        // Can assign values the usual way
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a.set(i, j, i + j + 1);
+            }
+        }
+
+        // Direct manipulation of each value is the fastest way to assign/read values
+        a.a11 = 12;
+        a.a23 = 64;
+
+        // can print the usual way too
+        a.print();
+
+        // most of the standard operations are support
+        CommonOps_DDF3.transpose(a, b);
+        b.print();
+
+        System.out.println("Determinant = " + CommonOps_DDF3.det(a));
     }
 }
